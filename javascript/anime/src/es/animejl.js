b4b67a807100ed681a8fa89cd326edaf1a46ed09
Freitez93 <Freitez_93@hotmail.com>
2025-03-05 23:19:33 -0400

    ♻️ refactor: se corrigio los filtros

--- javascript/anime/src/es/animejl.js
+++ javascript/anime/src/es/animejl.js
@@ -7,7 +7,7 @@ const mangayomiSources = [
 		"iconUrl": "https://www.anime-jl.net/favicon.ico",
 		"typeSource": "single",
 		"itemType": 1,
-		"version": "0.0.1",
+		"version": "0.0.2",
 		"dateFormat": "",
 		"dateFormatLocale": "",
 		"pkgPath": "anime/src/es/animejl.js"
